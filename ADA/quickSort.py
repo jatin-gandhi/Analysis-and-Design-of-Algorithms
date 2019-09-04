01c076f26f68601dc668443e134d812114c43d2c
Jatin Gandhi <45176848+jatin-gandhi@users.noreply.github.com>
2019-09-05 00:35:35 +0530

    Update quickSort.py

--- ADA/quickSort.py
+++ ADA/quickSort.py
@@ -1,31 +1,27 @@
-def partition (array, low,high):	
-	pivot = arr[high]
-	i = (low - 1) 
-	for j in range(low,high-1,1): 
-		if array[j] <= pivot: 
-			i=i+1 
-			temp=array[i]
-			array[i]=array[j]
-			array[j]=temp 
-	temp=array[i+1]
-	array[i+1]=array[high]
-	array[high]=temp
-	return (i + 1) 
- 
+def quickSort(array, low, high):
+    if low < high:
+        index = partition(array, low, high)
+        quickSort(array, low, index - 1)
+        quickSort(array, index + 1, high)
 
 
-def quickSort(array,low,high) :
-	if low < high :
-		pi = partition(arr, low, high) 
-		quickSort(arr, low, pi - 1) 
-		quickSort(arr, pi + 1, high) 
- 
+
+	
+def partition(array, low, high):
+    pivot = array[high]
+    i = low
+    for j in range(low, high):       
+        if (array[j] < pivot):
+            if i != j:
+                array[i], array[j] = array[j], array[i]
+            i += 1
+    array[i], array[high] = array[high], array[i]
+    return i
 
 
-n = int(input("Enter the size of the array"))
-arr = list(map(int,input("Enter the elements of array").split()))
-quickSort(arr, 0, n-1) 
-print("Sorted Array")
-for value in arr:
-	print(value,end=" ")	
 
+
+array = list(map(int, input("Enter numbers: ").strip(" ").split(" ")))
+size = len(array)
+quickSort(array, 0, size - 1)
+print("Sorted Array: ", array)
